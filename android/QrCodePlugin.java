400bc80f587d8a50692779c86a8df4a6f0710036
t1jsalazar <jsalazar@todo1.com>
2020-07-09 10:59:29 -0500

    change library swift

--- android/QrCodePlugin.java
+++ android/QrCodePlugin.java
@@ -1,3 +1,23 @@
+//**
+/**
+ * © TODO1 SERVICES, INC. (‘TODO1’) All rights reserved, 2000, 2018.
+ *
+ *  This work is protected by the United States of America copyright laws.
+ *
+ *  All information contained herein is and remains the property of
+ *  TODO1 [and its suppliers, if any] .
+ *  Dissemination of this information or reproduction of this material
+ *  is not permitted unless prior written consent is obtained
+ *  from TODO1 SERVICES, INC.
+ *
+ *  This work is protected by the United States of America copyright laws.
+ *
+ *  All information contained herein is and remains the property of
+ *  TODO1 [and its suppliers, if any] .
+ *  Dissemination of this information or reproduction of this material
+ *  is not permitted unless prior written consent is obtained
+ *  from TODO1 SERVICES, INC.
+ */
 package com.todo1.cordova.plugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
--- www/QrCodePlugin.js
+++ www/QrCodePlugin.js
@@ -1,10 +1,29 @@
-// Empty constructor
+//**
+/**
+ * © TODO1 SERVICES, INC. (‘TODO1’) All rights reserved, 2000, 2018.
+ *
+ *  This work is protected by the United States of America copyright laws.
+ *
+ *  All information contained herein is and remains the property of
+ *  TODO1 [and its suppliers, if any] .
+ *  Dissemination of this information or reproduction of this material
+ *  is not permitted unless prior written consent is obtained
+ *  from TODO1 SERVICES, INC.
+ *
+ *  This work is protected by the United States of America copyright laws.
+ *
+ *  All information contained herein is and remains the property of
+ *  TODO1 [and its suppliers, if any] .
+ *  Dissemination of this information or reproduction of this material
+ *  is not permitted unless prior written consent is obtained
+ *  from TODO1 SERVICES, INC.
+ */
 function QrCodePlugin() {}
 
 // The function that passes work along to native shells
 // Message, subject and qr are strings
 QrCodePlugin.prototype.shareqr = function(message, subject, qr, successCallback, errorCallback) {
-    var options = {};
+    var options = {}
     options.message = message;
     options.subject = subject;
     options.qr = qr;
